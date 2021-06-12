370dece09f85ea0809f6510003db5989a425e458
Saksham Kaushal <sakshamk.710@gmail.com>
2021-06-12 15:52:54 +0530

    Updated font scale for plots. Added new plot to tex file. Commented code. Rearraged functions and formatted code sections.

--- June7-hdf5Data/scripts/plotter.py
+++ June7-hdf5Data/scripts/plotter.py
@@ -9,6 +9,26 @@ import time
 
 # ===================================== User-defined function definitions ===============================
 
+# ----------------------- General-purpose functions -----------------------
+
+def capitalize_first_letter(string):
+	'''
+	Returns a string with capitalized first letter.
+	Parameters	:
+	string	- a string whose first words is to be capitalized. This does not return string in camelcase, for which we might need to call this function repeated on split string.
+	'''
+	return string[0].capitalize()+string[1:]
+
+	'''
+	Returns a list of dataframes with an extra column added, which signifies mode of assembly of data. This is denoted by name of dataframe.
+	Parameters	:
+	df_list	- a list of dataframes with their name attributes already set.
+	'''
+	for df in df_list :
+		df['assembly']	= df.name
+	return df_list
+
+# ----------------------- Getter functions --------------------------------
 
 def get_directory(directory):
 	'''
@@ -26,8 +46,6 @@ def get_directory(directory):
 	dir_dict['plots_dir']			= os.path.join(dir_dict['root_dir'], 'plots')
 	return dir_dict[directory+'_dir']
 
-		# ---------------------- Getter functions for hdf5 files dataset columns ---------------------
-
 def get_files(directory, mode='r'):
 	'''
 	Returns hdf5 file handles for all files in a directory.
@@ -37,6 +55,8 @@ def get_files(directory, mode='r'):
 	mode		- Mode of opening the file; defaults to read mode.
 	'''
 	return [h5py.File(os.path.join(directory,fname),mode) for fname in os.listdir(directory)]
+
+# ++++++++++++++++++++ Getter functions for hdf5 files dataset columns 
 
 def get_coords_x(fname):
 	'''
@@ -102,6 +122,8 @@ def get_redshift(fname):
 	'''
 	return float(re.compile(r'z\d+p\d+').findall(str(fname.file))[0].replace('z','').replace('p','.'))
 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 def get_subdf(fname,params_list):
 	'''
 	Returns a sub-dataframe containing a subset of columns available in a single hdf5 file.
@@ -137,38 +159,6 @@ def get_df(fname_list, params_list=['coords_x', 'coords_y', 'coords_z', 'vel_x',
 	df = pd.concat([df]+[get_subdf(fname,params_list) for fname in fname_list], ignore_index=True)
 	return df
 
-def capitalize_first_letter(string):
-	'''
-	Returns a string with capitalized first letter.
-	Parameters	:
-	string	- a string whose first words is to be capitalized. This does not return string in camelcase, for which we might need to call this function repeated on split string.
-	'''
-	return string[0].capitalize()+string[1:]
-
-def prepare_plot(context='paper',theme='dark',font_scale=1,rc_kwparams=dict()):
-	'''
-	Set seaborn styling for plots.
-	'''
-	rc_params	= {
-		'xtick.bottom': True, 
-		'ytick.left': True, 
-		'axes.spines.left': True, 
-		'axes.spines.bottom': True
-		}
-	rc_params.update(rc_kwparams)			# Update parameters by adding parameters passed to function
-	sns.set_context(context,font_scale)
-	sns.set_style(theme,rc_params)
-
-def add_assembly_column(df_list):
-	'''
-	Returns a list of dataframes with an extra column added, which signifies mode of assembly of data. This is denoted by name of dataframe.
-	Parameters	:
-	df_list	- a list of dataframes with their name attributes already set.
-	'''
-	for df in df_list :
-		df['assembly']	= df.name
-	return df_list
-
 def get_particle_distribution(df_list,col='redshift'):
 	'''
 	Returns a single dataframe containing value counts of different values of column "col".
@@ -184,6 +174,53 @@ def get_particle_distribution(df_list,col='redshift'):
 		df 					= pd.concat([df,count])
 	return df
 
+def get_total_mass_in_particles_with_redshift(df_list):
+	'''
+	Returns a concatenated dataframe containing redshift, sum of particle masses at a given redshift and assembly mode.
+	Parameters	:
+	df_list 	- List of dataframes (presumably, with different assembly modes) for which distribution is to be plotted.
+	'''
+	subdf_list 	= list()
+	for subdf in df_list:
+		grouped_df 			= subdf.groupby(['redshift'],as_index=False).agg({'mass':sum})
+		grouped_df.name 	= subdf.name
+		subdf_list.append(grouped_df)
+	add_assembly_column(subdf_list)
+	return pd.concat(subdf_list)
+
+# ----------------------- Plotter functions --------------------------------
+
+def prepare_plot(context='paper',theme='dark',font_scale=1,rc_kwparams=dict()):
+	'''
+	Set seaborn styling for plots.
+	'''
+	rc_params	= {
+		'xtick.bottom': True, 
+		'ytick.left': True, 
+		'axes.spines.left': True, 
+		'axes.spines.bottom': True
+		}
+	rc_params.update(rc_kwparams)			# Update parameters by adding parameters passed to function
+	sns.set_context(context,font_scale)
+	sns.set_style(theme,rc_params)
+
+def plot_or_not(show,plot_name=None,dpi=480):
+	'''
+	Function to switch between show and save methods for plots.
+	Parameters	:	
+	show		- Shows an 'interactive' plot if True, else saves the plot if False. If set to None, does nothing.
+	plot_name	- If set, plot is saved using plot_name if show is set to False. If show is set to False, yet plot_name is not provided, a random number is generated for filename.
+	'''
+	if show == True :
+		plt.show()
+	elif show == False :
+		if plot_name == None :
+			plot_name = np.random.randint(10000,99999)
+		plt.savefig(os.path.join(get_directory('plots'),plot_name+'.png'),
+			dpi=dpi,bbox_inches='tight')
+	elif show == None :
+		pass 
+
 def plot_particle_distribution(df_list, col='redshift',show=True):
 	'''
 	Plotter function to plot the distribution of star particles with respect to a variable (redshift, by default).
@@ -193,7 +230,7 @@ def plot_particle_distribution(df_list, col='redshift',show=True):
 	col 		- column with respect to which distribution is to be plotted. Redshift is used by default.
 	show 		- passed to plot_or_not() function to evaluate whether to show the plot or save it.
 	'''
-	prepare_plot()
+	prepare_plot(font_scale=1.25)
 	dist_df			= get_particle_distribution(df_list,col)
 	hue				= 'assembly'
 	g 				= sns.relplot(data=dist_df,
@@ -212,6 +249,31 @@ def plot_particle_distribution(df_list, col='redshift',show=True):
 	plot_or_not(show,plot_name='particle_distribution_wrt_'+col)
 	return  							# No return value. Plot is either shown or saved, or nothing is done.
 
+def plot_total_mass_in_particles_with_redshift(df_list,show=True):
+	'''
+	Plotter function to plot the distribution of total mass of star particles at a given redshift with respect to redshift.
+	Parameters :
+	df_list 	- List of dataframes (presumably, with different assembly modes) for which distribution is to be plotted using separate hues on the same plot.
+	show 		- passed to plot_or_not() function to evaluate whether to show the plot or save it.
+	'''
+	prepare_plot(font_scale=1.25)
+	dist_df 		= get_total_mass_in_particles_with_redshift(df_list)
+	hue 			= 'assembly'
+	g 				= sns.relplot(data=dist_df,
+								  x='redshift',
+								  y='mass',
+								  hue= hue,
+								  kind='scatter').set(xlabel='Redshift',
+								  ylabel='Total Mass $M_{\odot}$')		# Plots a scatter plot. To plot histogram instead, use displot with kind='hist'. Counts are then computed automatically.
+	g_axis_level 	= sns.lineplot(data=dist_df,
+								   x='redshift',
+								   y='mass',
+								   hue= hue,
+								   ax=g.ax,
+								   legend=False)		# Plots a line plot above which scatter points will lie.
+	g._legend.set_title(capitalize_first_letter(str(hue)))
+	plot_or_not(show,plot_name='total_mass_wrt_redshift')
+	return  							# No return value. Plot is either shown or saved, or nothing is done.
 
 def plot_mass_distribution(df_list,show=True):
 	'''
@@ -258,52 +320,6 @@ def plot_mass_distribution_with_redshift(df_list,show=True):
 		plot_or_not(show,plot_name='mass_distribution_wrt_redshift_'+str(df.name),dpi=240)
 	return
 
-def plot_or_not(show,plot_name=None,dpi=480):
-	'''
-	Function to switch between show and save methods for plots.
-	Parameters	:	
-	show		- Shows an 'interactive' plot if True, else saves the plot if False. If set to None, does nothing.
-	plot_name	- If set, plot is saved using plot_name if show is set to False. If show is set to False, yet plot_name is not provided, a random number is generated for filename.
-	'''
-	if show == True :
-		plt.show()
-	elif show == False :
-		if plot_name == None :
-			plot_name = np.random.randint(10000,99999)
-		plt.savefig(os.path.join(get_directory('plots'),plot_name+'.png'),
-			dpi=dpi,bbox_inches='tight')
-	elif show == None :
-		pass 
-
-def get_total_mass_in_particles_with_redshift(df_list):
-	subdf_list 	= list()
-	for subdf in df_list:
-		grouped_df 			= subdf.groupby(['redshift'],as_index=False).agg({'mass':sum})
-		grouped_df.name 	= subdf.name
-		subdf_list.append(grouped_df)
-	add_assembly_column(subdf_list)
-	return pd.concat(subdf_list)
-
-def plot_total_mass_in_particles_with_redshift(df_list,show=True):
-	prepare_plot()
-	dist_df 		= get_total_mass_in_particles_with_redshift(df_list)
-	hue 			= 'assembly'
-	g 				= sns.relplot(data=dist_df,
-								  x='redshift',
-								  y='mass',
-								  hue= hue,
-								  kind='scatter').set(xlabel='Redshift',
-								  ylabel='Total Mass $M_{\odot}$')		# Plots a scatter plot. To plot histogram instead, use displot with kind='hist'. Counts are then computed automatically.
-	g_axis_level 	= sns.lineplot(data=dist_df,
-								   x='redshift',
-								   y='mass',
-								   hue= hue,
-								   ax=g.ax,
-								   legend=False)		# Plots a line plot above which scatter points will lie.
-	g._legend.set_title(capitalize_first_letter(str(hue)))
-	plot_or_not(show,plot_name='total_mass_wrt_redshift')
-	return  							# No return value. Plot is either shown or saved, or nothing is done.
-
 
 # ============================================ Main program =============================================
 
@@ -338,10 +354,10 @@ if __name__ == '__main__' :
 
 	# ------- Plot the particle number distribution for all assembly modes.
 
-	# plot_particle_distribution(df_list,show=True)
-
 	# plot_mass_distribution(df_list,show=True)
 
 	# plot_mass_distribution_with_redshift(df_list,show=True)
+
+	# plot_particle_distribution(df_list,show=True)
 
 	# plot_total_mass_in_particles_with_redshift(df_list,show=True)
